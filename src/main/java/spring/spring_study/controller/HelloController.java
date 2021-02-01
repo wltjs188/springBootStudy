6c1f3f860b8c3992d38683268b317bf5ebcea476
DESKTOP-BBQAPPJ\user <wltjs188@gmail.com>
2021-02-01 17:53:02 +0900

    API

--- src/main/java/spring/spring_study/controller/HelloController.java
+++ src/main/java/spring/spring_study/controller/HelloController.java
@@ -3,6 +3,8 @@ package spring.spring_study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,34 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!"); //키-data, data-"hello!!"
         return "hello"; //resources:templets->(ViewName)hello.html 로 매핑
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //뷰없이 데이터가 바로 감
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; //뷰없이 데이터가 바로 감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체넘김(json)
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
