2e187a0481c4e783a5da63106d0bf3fbd0e0f9aa
DESKTOP-BBQAPPJ\user <wltjs188@gmail.com>
2021-02-15 18:13:17 +0900

    순수 jdbc

--- src/main/java/spring/spring_study/SpringConfig.java
+++ src/main/java/spring/spring_study/SpringConfig.java
@@ -1,21 +1,33 @@
 package spring.spring_study;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.spring_study.domain.Member;
+import spring.spring_study.repository.JdbcMemberRepository;
 import spring.spring_study.repository.MemberRepository;
 import spring.spring_study.repository.MemoryMemberRepository;
 import spring.spring_study.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration //자바 코드로 직접 스프링 빈 등록
 public class SpringConfig {
-    
+
+    private DataSource dataSource; 
+
+    @Autowired //스프링이 자체적으로 DataSource 넣어줌
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
